059ac00bfde6292fac0c6eb1764311c7097d7620
manami-sato <manamisato2001@gmail.com>
2022-12-23 23:55:53 +0900

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <a href="/get">get</a>
+      <br />
+      <br />
+      <a href="/set">set</a>
+    </div>
+  );
 };
 
 export default Home;
