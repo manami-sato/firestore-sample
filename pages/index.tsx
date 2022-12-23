5335be6948040a6e910afa0a61c598beea578c7e
manami-sato <manamisato2001@gmail.com>
2022-12-23 19:58:58 +0900

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,15 @@
-import {
-  addDoc,
-  getDocs,
-  getDoc,
-  collection,
-  doc,
-  Firestore,
-} from 'firebase/firestore';
 import type { NextPage } from 'next';
-import { SetStateAction, useState } from 'react';
-import db from '../src/libs/firebase';
+import firebaseApp from '../src/libs/firebase';
 
 const Home: NextPage = () => {
-  const [nameText, setNameText] = useState('');
-
-  const aaa = async (db: Firestore) => {
-    const citiesCol = collection(db, 'hoge');
-    const citySnapshot = await getDocs(citiesCol);
-    const cityList = citySnapshot.docs.map((doc) => doc.data());
-    console.log(cityList);
-
-    return cityList;
-  };
-  aaa(db);
-
-  const onChangeName = (event: {
-    target: { value: SetStateAction<string> };
-  }) => {
-    setNameText(event.target.value);
-  };
-
-  const onClickAdd = async () => {
-    console.log('Ok');
-
-    try {
-      const docRef = await addDoc(collection(db, 'hoge'), {
-        name: nameText,
-      });
-      console.log('Document written with ID: ', docRef.id);
-    } catch (e) {
-      console.error('Error adding document: ', e);
-    }
-  };
+  const db = firebaseApp();
+  console.log(db);
 
   return (
-    <div className="App">
-      <input type="text" value={nameText} onChange={onChangeName} />
-      <button onClick={() => onClickAdd()}>追加</button>
+    <div>
+      <div></div>
     </div>
   );
 };
 
 export default Home;
-
-export const getStaticProps = async () => {
-  const citiesCol = collection(db, 'hoge');
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map((doc) => doc.data());
-  console.log(cityList);
-  return {
-    props: {
-      cityList: cityList,
-    },
-  };
-};
--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,19 +1,28 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { firebaseConfig } from './firebaseConfig';
+import {
+  collection,
+  getDocs,
+  getFirestore,
+} from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || '',
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
-  projectId: process.env.FIREBASE_PROJECT_ID || '',
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
-  measurementId: process.env.FIREBASE_APP_ID || '',
-};
+const firebaseApp = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const TARGET_COLLECTION_NAME = 'users';
 
-export default db;
+export default async function () {
+  try {
+    const db = getFirestore(firebaseApp);
+    // const q = collection(db, TARGET_COLLECTION_NAME);
+    const querySnapshot = await getDocs(collection(db, TARGET_COLLECTION_NAME));
+    const ret: any = [];
+    querySnapshot.forEach((doc) => {
+      ret.push(doc.data());
+    });
+    return ret;
+  } catch (error) {
+    console.log('error');
+  }
+}
